0141180958aecbab7adae0c9aff0d170d71aed26
Vivek V Kini <42902325+kiniv-21@users.noreply.github.com>
2026-03-06 14:42:34 +0530

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -7,12 +7,12 @@ export const products: Product[] = [
     price: 350,
     description: 'Hand-painted floral design on a sturdy jute tote bag with an attached pocket. Perfect for everyday use with internal pockets and strong handles. Dimensions: Height: 12", Width: 14", Base: 6". Price inclusive of delivery.',
     images: [
-      'https://drive.google.com/file/d/1v7Va9SOS4auThxk8ak0590qT-E4UcFcC/view?usp=drive_link',
-      'https://drive.google.com/file/d/1Wk_lYA6JKCMOuHkYcdpIjFboxIVIxF-4/view?usp=drive_link',
-      'https://drive.google.com/file/d/11pfA-n_Rd5zPxHBkFvDiG12td_vi9-Bf/view?usp=drive_link',
-      'https://drive.google.com/file/d/1JSWnqpADci3LNpe6q1HweNLxe8okDn2n/view?usp=drive_link',
-      'https://drive.google.com/file/d/12UkbPJdt1-cv02-baJNahCEh1-kQxKos/view?usp=drive_link',
-      'https://drive.google.com/file/d/17xUS1bKakZOo1ebdnYwf2ssbnpaYaSzT/view?usp=drive_link'
+      'https://drive.google.com/uc?export=view&id=1v7Va9SOS4auThxk8ak0590qT-E4UcFcC',
+      'https://drive.google.com/uc?export=view&id=1Wk_lYA6JKCMOuHkYcdpIjFboxIVIxF-4',
+      'https://drive.google.com/uc?export=view&id=11pfA-n_Rd5zPxHBkFvDiG12td_vi9-Bf',
+      'https://drive.google.com/uc?export=view&id=1JSWnqpADci3LNpe6q1HweNLxe8okDn2n',
+      'https://drive.google.com/uc?export=view&id=12UkbPJdt1-cv02-baJNahCEh1-kQxKos',
+      'https://drive.google.com/uc?export=view&id=17xUS1bKakZOo1ebdnYwf2ssbnpaYaSzT'
     ],
     category: 'totes',
     featured: true,
@@ -25,14 +25,14 @@ export const products: Product[] = [
     price: 350,
     description: 'Serene floral patterns hand-painted on a medium-sized jute tote bag.',
     images: [
-      'https://drive.google.com/file/d/1fhdK-aT3fMRm3A4jUZWz1NwAQN7IBA-D/view?usp=drive_link',
-      'https://drive.google.com/file/d/1VvswVV7nyjHaEWNwsmWHE7WI925Ds53k/view?usp=drive_link',
-      'https://drive.google.com/file/d/1q66VfOVuhHfKmLCpkpA3qw1Wa16H6iNh/view?usp=drive_link',
-      'https://drive.google.com/file/d/16ZGiHVgMa_w3y3zeOzOdyX7BFkuhnHbu/view?usp=drive_link',
-      'https://drive.google.com/file/d/1psT48s90184_9ijouaxt7xVIImDxAFLA/view?usp=drive_link',
-      'https://drive.google.com/file/d/1wtkkAPrAFaCFQfqdxABkHFMl7DZZ9mOh/view?usp=drive_link',
-      'https://drive.google.com/file/d/1SBbhnTl9qO2YhHTM_dYtiFVazD_Xi0Kk/view?usp=drive_link',
-      'https://drive.google.com/file/d/1rN2jMBc_i0sWBy9sZ6DWhl47WmDHi-Zc/view?usp=drive_link'
+      'https://drive.google.com/uc?export=view&id=1fhdK-aT3fMRm3A4jUZWz1NwAQN7IBA-D',
+      'https://drive.google.com/uc?export=view&id=1VvswVV7nyjHaEWNwsmWHE7WI925Ds53k',
+      'https://drive.google.com/uc?export=view&id=1q66VfOVuhHfKmLCpkpA3qw1Wa16H6iNh',
+      'https://drive.google.com/uc?export=view&id=16ZGiHVgMa_w3y3zeOzOdyX7BFkuhnHbu',
+      'https://drive.google.com/uc?export=view&id=1psT48s90184_9ijouaxt7xVIImDxAFLA',
+      'https://drive.google.com/uc?export=view&id=1wtkkAPrAFaCFQfqdxABkHFMl7DZZ9mOh',
+      'https://drive.google.com/uc?export=view&id=1SBbhnTl9qO2YhHTM_dYtiFVazD_Xi0Kk',
+      'https://drive.google.com/uc?export=view&id=1rN2jMBc_i0sWBy9sZ6DWhl47WmDHi-Zc'
     ],
     category: 'shoulder',
     featured: true,
@@ -45,12 +45,12 @@ export const products: Product[] = [
     price: 39.99,
     description: 'Bold abstract designs on a compact jute file folder.',
     images: [
-      'https://drive.google.com/file/d/1IgNTaHOHiwVPXFHblXHUgJ0B2tVcCFA5/view?usp=drive_link',
-      'https://drive.google.com/file/d/117dlFoULqNZ-RIKjpD5H-X7xQWCpYumK/view?usp=drive_link',
-      'https://drive.google.com/file/d/1XpEdx2h3db9xLjUXGtq6C2vz-3OA8gdR/view?usp=drive_link',
-      'https://drive.google.com/file/d/18GeFC2MGcMdzS7HOieVeA4a9uo9OgN1k/view?usp=drive_link',
-      'https://drive.google.com/file/d/1XoYv-xHHadKgo-ee5XAJjlFSLIfwPuii/view?usp=drive_link',
-      'https://drive.google.com/file/d/1qeBr2gUaWRWnpM_HkL8qukka_9cr3Muc/view?usp=drive_link'
+      'https://drive.google.com/uc?export=view&id=1IgNTaHOHiwVPXFHblXHUgJ0B2tVcCFA5',
+      'https://drive.google.com/uc?export=view&id=117dlFoULqNZ-RIKjpD5H-X7xQWCpYumK',
+      'https://drive.google.com/uc?export=view&id=1XpEdx2h3db9xLjUXGtq6C2vz-3OA8gdR',
+      'https://drive.google.com/uc?export=view&id=18GeFC2MGcMdzS7HOieVeA4a9uo9OgN1k',
+      'https://drive.google.com/uc?export=view&id=1XoYv-xHHadKgo-ee5XAJjlFSLIfwPuii',
+      'https://drive.google.com/uc?export=view&id=1qeBr2gUaWRWnpM_HkL8qukka_9cr3Muc'
     ],
     category: 'clutch',
     featured: false,
@@ -63,11 +63,11 @@ export const products: Product[] = [
     price: 400,
     description: 'Foldable trays',
     images: [
-      'https://drive.google.com/file/d/1hUqGEvddIh_Cp1HuZISlSU4OaXg_0OY5/view?usp=drive_link',
-      'https://drive.google.com/file/d/14EYAljFcHzFoVEr26L8CkvktLAs5GQih/view?usp=drive_link',
-      'https://drive.google.com/file/d/1ZVFsULNdxGlguXLp5syYzb_JkZQ9nnkb/view?usp=drive_link',
-      'https://drive.google.com/file/d/19r6zm7YKCTwo-wWGZh6u7l-1vOQewoEn/view?usp=drive_link',
-      'https://drive.google.com/file/d/1SHrj8ebmZlaJZFPa71qr_dzK662x6H6U/view?usp=drive_link'
+      'https://drive.google.com/uc?export=view&id=1hUqGEvddIh_Cp1HuZISlSU4OaXg_0OY5',
+      'https://drive.google.com/uc?export=view&id=14EYAljFcHzFoVEr26L8CkvktLAs5GQih',
+      'https://drive.google.com/uc?export=view&id=1ZVFsULNdxGlguXLp5syYzb_JkZQ9nnkb',
+      'https://drive.google.com/uc?export=view&id=19r6zm7YKCTwo-wWGZh6u7l-1vOQewoEn',
+      'https://drive.google.com/uc?export=view&id=1SHrj8ebmZlaJZFPa71qr_dzK662x6H6U'
     ],
     category: 'totes',
     featured: true,
@@ -80,11 +80,11 @@ export const products: Product[] = [
     price: 350,
     description: 'Kalamkari Hand Painted shoulder bag',
     images: [
-      'https://drive.google.com/file/d/11bqYdroah9Nd7KMXoSQtEWwHcWVZb7P_/view?usp=drive_link',
-      'https://drive.google.com/file/d/18RA24y1ZJJGgqZ_j5diTAWXObz8fTQgu/view?usp=drive_link',
-      'https://drive.google.com/file/d/1Jknxezumddyp_1YLWQEbNWob5ZgHo55h/view?usp=drive_link',
-      'https://drive.google.com/file/d/1YTVRTPrrzi_YvWw4fOA7VjsBL_ywBFyf/view?usp=drive_link',
-      'https://drive.google.com/file/d/1HVpy9chg5Vr6SLkVH5EvEciy-p_-SKCs/view?usp=drive_link'
+      'https://drive.google.com/uc?export=view&id=11bqYdroah9Nd7KMXoSQtEWwHcWVZb7P_',
+      'https://drive.google.com/uc?export=view&id=18RA24y1ZJJGgqZ_j5diTAWXObz8fTQgu',
+      'https://drive.google.com/uc?export=view&id=1Jknxezumddyp_1YLWQEbNWob5ZgHo55h',
+      'https://drive.google.com/uc?export=view&id=1YTVRTPrrzi_YvWw4fOA7VjsBL_ywBFyf',
+      'https://drive.google.com/uc?export=view&id=1HVpy9chg5Vr6SLkVH5EvEciy-p_-SKCs'
     ],
     category: 'messenger',
     featured: false,
@@ -97,11 +97,11 @@ export const products: Product[] = [
     price: 300,
     description: 'Vibrant sunset-inspired design on a compact jute tote. Perfect size for essentials with secure zip closure.',
     images: [
-      'https://drive.google.com/file/d/1E7V3J7YOgVajLdvBwQlUobxUFQukuUxl/view?usp=drive_link',
-      'https://drive.google.com/file/d/10sSRmupPF4K7j10kWm1zjNPzq4lKKi2f/view?usp=drive_link',
-      'https://drive.google.com/file/d/1MGgV7NYdavTcUO4f9hrUDsWJFihWLyx6/view?usp=drive_link',
-      'https://drive.google.com/file/d/1jEoY43vQyWFGmPvr0rAvKBhDeHnAf3HA/view?usp=drive_link',
-      'https://drive.google.com/file/d/1ItH5vy1IQmQgqhkmfwTgXUxgVtBZ_TgX/view?usp=drive_link'
+      'https://drive.google.com/uc?export=view&id=1E7V3J7YOgVajLdvBwQlUobxUFQukuUxl',
+      'https://drive.google.com/uc?export=view&id=10sSRmupPF4K7j10kWm1zjNPzq4lKKi2f',
+      'https://drive.google.com/uc?export=view&id=1MGgV7NYdavTcUO4f9hrUDsWJFihWLyx6',
+      'https://drive.google.com/uc?export=view&id=1jEoY43vQyWFGmPvr0rAvKBhDeHnAf3HA',
+      'https://drive.google.com/uc?export=view&id=1ItH5vy1IQmQgqhkmfwTgXUxgVtBZ_TgX'
     ],
     category: 'totes',
     featured: false,
