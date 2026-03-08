af528d2d783fee2631d5c50ace84ccfa0d0dc65f
kiniv-21 <vivekvkini798@gmail.com>
2026-03-08 16:25:50 +0530

    admin page fallback

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import { Admin } from './pages/Admin.tsx';
 import './index.css';
 
-const isAdminPath = window.location.pathname.endsWith('/admin');
+const isAdminPath = /\/admin\/?$/.test(window.location.pathname);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
